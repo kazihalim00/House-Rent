519cb31b6a65682453c49b9b25beefd8ce078d31
Tasnima <shraboniakter554@gmail.com>
2026-06-11 13:00:23 +0600

    if house booked then it will not show in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -461,9 +461,17 @@ class HomeController extends Controller
         return back()->with('success', 'Review deleted successfully!');
 
     }
+
+    // if house booked then it will not show in home page
     public function overview()
     {
-        $houses = Home::where('status', 'approved')->latest()->take(6)->get();
+        $houses = Home::where('status', 'approved')
+            ->whereDoesntHave('bookings', function ($q) {
+                $q->whereRaw('DATE_ADD(check_in_date, INTERVAL booking_duration MONTH) > ?', [Carbon::now()->toDateString()]);
+            })
+            ->latest()
+            ->take(6)
+            ->get();
         $reviews = Review::whereNull('house_id')->with('user')->latest()->take(6)->get();
 
         return view('frontend.pages.home', compact('houses', 'reviews'));
